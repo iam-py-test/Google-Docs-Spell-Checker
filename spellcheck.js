fab70602996b61f521bcd6d959af5d24d19161c4
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-05-18 13:10:09 -0400

    Update spellcheck.js
    Add comments and remove unneeded code

--- spellcheck.js
+++ spellcheck.js
@@ -1,10 +1,17 @@
+//use strict to make this code safer
 "use strict";
+//query all the text
 var q = document.querySelectorAll('.kix-wordhtmlgenerator-word-node')
+//make var to store the text
 var txt = ''
+//loop throught each of the elements adding their text content
 q.forEach(function(e){txt += e.innerText})
-var split1 = txt;
+//log the text
 console.log(txt,q)
+//get all the links - just for testing purpuses - this extention does not use this but you could make one that does
 var links = document.querySelectorAll("a.kix-link")
+//log how many links there are
 console.log(links.length)
-split1;
+//return the text
+txt;
 		
